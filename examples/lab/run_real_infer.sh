2a7656d6f7e60dc5606b71405b461164596615fa
qiming <ceilingfan0924@gmail.com>
2026-02-27 18:55:53 +0800

    Add all experiment results for now

--- examples/lab/run_real_infer.sh
+++ examples/lab/run_real_infer.sh
@@ -1,14 +1,39 @@
 # cd /home/showlab/VLASafety/openpi-main/examples/lab
 
 
+# ----------------------------------------------------------------------------
+## experiment ?
+## ???
+## observations: 
+## 
+# CKPT_CFG="pi05_lab_finetune_orange_cube_single_point_dual_external_views"
+# MODEL_NAME="$CKPT_CFG/1999"
+# ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_dual_external_views_02_27"
+# ----------------------------------------------------------------------------
 
 
 ############### experiments on views  #########################
 
 
 # ----------------------------------------------------------------------------
+## experiment 7
+## comparison with experiment 2 to see if we can only use one view. 
+## observations: similar observations as the one with three views. it just goes down? and start opening and close. why does it always go
+##              down after it starts. what is it actually learning?
+## 
+# CKPT_CFG="pi05_lab_finetune_orange_cube_single_point_single_base_view"
+# MODEL_NAME="$CKPT_CFG/1999"
+# ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_single_base_view_02_27"
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
 ## experiment 6
-## comparison with experiment 2 to see whether we can train with three views.
+## comparison with experiment 2 to see whether we can train with two external views.
+## observations: this one totally fails as well. it is ignoring the cube. but the actions are regressed somewhat correctly. 
+##              this is interesting because it sort of shows, the action expert is overfitting really quickly? it is not learning the 
+##              the connections between images and actions correctly. it is just memorising the actions. 
+## 
 # CKPT_CFG="pi05_lab_finetune_orange_cube_single_point_dual_external_views"
 # MODEL_NAME="$CKPT_CFG/1999"
 # ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_dual_external_views_02_27"
@@ -18,6 +43,10 @@
 # ----------------------------------------------------------------------------
 ## experiment 5
 ## comparison with experiment 2 to see whether we can train with three views.
+## observations: this one totally fails worse than the previous ones. the gripper just goes down vertically without any other movement. 
+##              this is a bit strange. since it doesnt seem to be overfitting. the actions are totally wrong. 
+##              i think this is mainly because we have trained on a new view so need more training. 
+## 
 # CKPT_CFG="pi05_lab_finetune_orange_cube_single_point_three_views"
 # MODEL_NAME="$CKPT_CFG/1999"
 # ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_three_views_02_27"
@@ -34,6 +63,9 @@
 # ----------------------------------------------------------------------------
 ## experiment 4
 ## comparison with experiment 2 to see how much training demonstrations we need.  
+## observations: can grasp correctly. but placement still have some issue. not so extreme to the point where it places at the corner. 
+##              but doesnt place it onto the centre of the coaster still. 
+##
 # CKPT_CFG="pi05_lab_finetune_orange_cube_single_point_15"
 # MODEL_NAME="$CKPT_CFG/1999"
 # ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_15_02_27"
@@ -43,6 +75,9 @@
 # ----------------------------------------------------------------------------
 ## experiment 3
 ## comparison with experiment 2 to see how much training demonstrations we need.  
+## observations: can grasp not so correctly. will miss on the first try and sometimes succeed on the second try. 
+##              the placement is completely bad. can will place the cube at the far corner of the table near the robot side. 
+## 
 # CKPT_CFG="pi05_lab_finetune_orange_cube_single_point_10"
 # MODEL_NAME="$CKPT_CFG/1999"
 # ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_10_02_27"
@@ -52,9 +87,9 @@
 # ----------------------------------------------------------------------------
 ## experiment 2
 ## retrain of the original dataset with the new code for sanity chceks.
-CKPT_CFG="pi05_lab_finetune_orange_cube_single_point"
-MODEL_NAME="$CKPT_CFG/1999"
-ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_02_27"
+# CKPT_CFG="pi05_lab_finetune_orange_cube_single_point"
+# MODEL_NAME="$CKPT_CFG/1999"
+# ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_single_point_02_27"
 # ----------------------------------------------------------------------------
 
 
@@ -71,9 +106,9 @@ ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_si
 ## this one has been used as a snaity check baseline
 ## orange cube baseline dataset with 25 episodes, it uses front camera
 ## this one only has two views
-# CKPT_CFG="pi05_lab_finetune_orange_cube_single_point"
-# MODEL_NAME="$CKPT_CFG/1999"
-# ROOT_PATH="/home/eva-01/code/openpi/checkpoints/lab_training_orange_cube_single_point"
+CKPT_CFG="pi05_lab_finetune_orange_cube_single_point"
+MODEL_NAME="$CKPT_CFG/1999"
+ROOT_PATH="/home/eva-01/code/openpi/checkpoints/baselines/lab_training_orange_cube_single_point"
 # ----------------------------------------------------------------------------
 
 
@@ -83,7 +118,7 @@ ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05_lab_finetune_orange_cube_si
 ## it uses side camera
 # CKPT_CFG="pi05_lab_finetune"
 # MODEL_NAME="$CKPT_CFG/2999"
-# ROOT_PATH="/home/eva-01/code/openpi/checkpoints/pi05-lab-finetune-2999"
+# ROOT_PATH="/home/eva-01/code/openpi/checkpoints/baselines/pi05-lab-finetune-2999"
 # ----------------------------------------------------------------------------
 
 
